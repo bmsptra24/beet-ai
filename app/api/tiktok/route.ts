381dab340b8eb7b7cd3aee09cac973c362c421a3
Bima Saputra <sbima2432@gmail.com>
2023-10-14 11:19:20 +0700

    #47 feat: connect to tiktok live chat

--- app/api/tiktok/route.ts
+++ app/api/tiktok/route.ts
@@ -1,7 +1,7 @@
-// // import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 // // import { WebcastPushConnection } from 'tiktok-live-connector'
 
-// // export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {}
 // //   const livestreamId: string = 'adisetiawansepuluh'
 // //   // Username of someone who is currently live
 // //   let tiktokUsername = livestreamId
