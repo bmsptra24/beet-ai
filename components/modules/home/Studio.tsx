71ebbf6b4e2cd3a2cbbe6646a81b79f415f45620
Bima Saputra <sbima2432@gmail.com>
2023-10-18 13:14:19 +0700

    #53 bug: if project empty || 14-c549cbf1725c4628.js:1 TypeError: Cannot read properties of undefined (reading '0') at b (page-46a8a08cd8a54dad.js:1:1796) at rv (bf6a786c-a8aa754092e011af.js:9:39640) at oT (bf6a786c-a8aa754092e011af.js:9:115202) at uG (bf6a786c-a8aa754092e011af.js:9:93373) at bf6a786c-a8aa754092e011af.js:9:93195 at uX (bf6a786c-a8aa754092e011af.js:9:93202) at n2 (bf6a786c-a8aa754092e011af.js:9:37226) at n4 (bf6a786c-a8aa754092e011af.js:9:37903) at bf6a786c-a8aa754092e011af.js:9:38955

--- components/modules/home/Studio.tsx
+++ components/modules/home/Studio.tsx
@@ -1,57 +1,57 @@
-import Message from '@/components/elements/home/Message'
-import Queue from '@/components/elements/home/Queue'
-import { RootState } from '@/store/store'
-import { ytMessageDummy } from '@/utils/dummyData'
-import { ytGetLiveChat } from '@/utils/services/youtube'
-import { AudioPlayer } from '@/utils/sound'
+import Message from "@/components/elements/home/Message";
+import Queue from "@/components/elements/home/Queue";
+import { RootState } from "@/store/store";
+import { ytMessageDummy } from "@/utils/dummyData";
+import { ytGetLiveChat } from "@/utils/services/youtube";
+import { AudioPlayer } from "@/utils/sound";
 import React, {
   useRef,
   Dispatch,
   SetStateAction,
   useEffect,
   useState,
-} from 'react'
-import { useSelector } from 'react-redux'
-import Header from './Header'
-import { textToSpeech } from '@/utils/tts'
+} from "react";
+import { useSelector } from "react-redux";
+import Header from "./Header";
+import { textToSpeech } from "@/utils/tts";
 // import { tiktokLiveChat } from '@/utils/services/tiktok'
-import { LiveChatMessage } from '@/types/types'
-import axios from 'axios'
-import { baseurl } from '@/lib/url'
+import { LiveChatMessage } from "@/types/types";
+import axios from "axios";
+import { baseurl } from "@/lib/url";
 // import { tiktokLiveChat } from '@/utils/services/tiktok'
 import {
   TikTokComponent,
   closeConnection,
   createConnection,
-} from '../tiktok/TikTokComponent'
-import { ButtonThin } from '@/components/elements/Button'
-import { generateAiAnswer } from '@/utils/openai'
+} from "../tiktok/TikTokComponent";
+import { ButtonThin } from "@/components/elements/Button";
+import { generateAiAnswer } from "@/utils/openai";
 type Props = {
-  setIsMenuOpen: Dispatch<SetStateAction<boolean>>
-}
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+};
 const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
-  const isTiktokConected = useRef(false)
-  const [mode, setMode] = useState<'auto' | 'semiauto'>('semiauto')
+  const isTiktokConected = useRef(false);
+  const [mode, setMode] = useState<"auto" | "semiauto">("semiauto");
   const [currAnswer, setCurrAnswer] = useState<{
-    author: string
-    message: string
-  }>()
+    author: string;
+    message: string;
+  }>();
   const [messages, setMessages] = useState<
     {
-      author: string
-      message: string
+      author: string;
+      message: string;
     }[]
-  >([])
+  >([]);
   const [editAnswer, setEditAnswer] = useState({
-    author: '',
-    message: '',
-  })
+    author: "",
+    message: "",
+  });
   const [queues, setQueues] = useState<
     {
-      author: string
-      message: string
+      author: string;
+      message: string;
     }[]
-  >([])
+  >([]);
 
   const {
     aiKnowlagge,
@@ -63,8 +63,8 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
     livestreamingId,
     mood,
     platform,
-  } = useSelector((state: RootState) => state.currProject)
-  const props = useSelector((state: RootState) => state.currProject)
+  } = useSelector((state: RootState) => state.currProject);
+  const props = useSelector((state: RootState) => state.currProject);
   // console.log({
   //   aiKnowlagge,
   //   aiRole,
@@ -78,32 +78,32 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
   // })
 
   useEffect(() => {
-    if (platform === 'tiktok' && isTiktokConected.current === false) {
-      createConnection(livestreamingId)
-      isTiktokConected.current = true
+    if (platform === "tiktok" && isTiktokConected.current === false) {
+      createConnection(livestreamingId);
+      isTiktokConected.current = true;
     }
 
     // return () => {
     //   closeConnection()
     // }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    if (platform === 'youtube') {
-      console.log('get chat live')
+    if (platform === "youtube") {
+      console.log("get chat live");
       const fetchData = async () => {
         const response: LiveChatMessage[] | null = await ytGetLiveChat(
           livestreamingId,
-          10,
-        )
+          10
+        );
 
-        if (response === null) return
+        if (response === null) return;
 
-        setMessages(response)
+        setMessages(response);
 
         response.map(async (msg, index) => {
-          const chat = { author: msg.author, message: msg.message }
-          console.log('get ai answer')
+          const chat = { author: msg.author, message: msg.message };
+          console.log("get ai answer");
 
           const response = await generateAiAnswer(
             chat,
@@ -112,9 +112,9 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
             livestreamTopic,
             mood,
             language,
-            aiKnowlagge,
-          )
-          console.log('add queue auto')
+            aiKnowlagge
+          );
+          console.log("add queue auto");
           setQueues([
             ...queues,
             {
@@ -124,49 +124,50 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
                 (response?.data as string) ||
                 (response?.body?.content as string),
             },
-          ])
-        })
-      }
+          ]);
+        });
+      };
 
-      const interval = setInterval(fetchData, 10000)
+      const interval = setInterval(fetchData, 10000);
 
       return () => {
-        clearInterval(interval)
-      }
+        clearInterval(interval);
+      };
     }
 
-    if (platform === 'tiktok') {
+    if (platform === "tiktok") {
       const fetchData = async () => {
-        if (mode === 'semiauto') return
-        if (messages.length === 0) return
-        if (queues.length > 5) return
+        if (mode === "semiauto") return;
+        if (messages.length === 0) return;
+        if (queues.length > 5) return;
 
-        console.log('cracked')
+        console.log("cracked");
 
         const chat = {
           author: messages[0]?.author,
           message: messages[0]?.message,
-        }
+        };
 
-        console.log('get ai answer')
+        console.log("get ai answer");
 
-        // const response = await generateAiAnswer(
-        //   chat,
-        //   avatarName,
-        //   aiRole,
-        //   livestreamTopic,
-        //   mood,
-        //   language,
-        //   aiKnowlagge,
-        // )
+        const response = await generateAiAnswer(
+          chat,
+          avatarName,
+          aiRole,
+          livestreamTopic,
+          mood,
+          language,
+          aiKnowlagge
+        );
 
-        const response: any = { data: 'DUmmy response' }
-        if (response === null) return
+        // const response: any = { data: 'DUmmy response' }
 
-        if (messages.length > 1) setMessages((prev) => prev.slice(1))
-        if (messages.length <= 1) setMessages([])
+        if (response === null) return;
 
-        console.log('add queue auto')
+        if (messages.length > 1) setMessages((prev) => prev.slice(1));
+        if (messages.length <= 1) setMessages([]);
+
+        console.log("add queue auto");
 
         const newAnswer = {
           author: messages[0]?.author,
@@ -174,20 +175,23 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
             (response?.content as string) ||
             (response?.data as string) ||
             (response?.body?.content as string),
-        }
+        };
 
-        setQueues((prev) => [...prev, newAnswer])
-        setCurrAnswer(newAnswer)
-      }
-      fetchData()
+        setQueues((prev) => [...prev, newAnswer]);
+        setCurrAnswer(newAnswer);
 
-      TikTokComponent(setMessages, setQueues, props)
+        // solusi sementara untuk mengatasi pesan yang duplikat
+        setMessages([]);
+      };
+      fetchData();
+
+      TikTokComponent(setMessages, setQueues, props);
     }
 
-    return () => {
-      setMessages([])
-    }
-  }, [livestreamingId, mode, currAnswer])
+    // return () => {
+    //   setMessages([]);
+    // };
+  }, [livestreamingId, mode, currAnswer, messages.length === 0]);
 
   // console.log({ messages })
 
@@ -206,7 +210,7 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
                   message={message}
                   key={index}
                 />
-              )
+              );
             })}
           </div>
         </section>
@@ -215,7 +219,7 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
             <p className="text-3xl font-bold">Review</p>
             <textarea
               className={`${
-                mode === 'auto' ? 'cursor-not-allowed' : 'cursor-text'
+                mode === "auto" ? "cursor-not-allowed" : "cursor-text"
               } mt-3 border-2 border-primary-black rounded px-2 py-1 bg-transparent`}
               placeholder="edit answer"
               onChange={(event) =>
@@ -229,17 +233,17 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
               id="review"
               cols={30}
               rows={10}
-              disabled={mode === 'auto'}
+              disabled={mode === "auto"}
             ></textarea>
             <button
-              disabled={queues.length >= 5 || mode === 'auto'}
+              disabled={queues.length >= 5 || mode === "auto"}
               onClick={() => {
-                if (editAnswer.message === '') return
-                setEditAnswer({ author: '', message: '' })
-                setQueues([...queues, editAnswer])
+                if (editAnswer.message === "") return;
+                setEditAnswer({ author: "", message: "" });
+                setQueues([...queues, editAnswer]);
               }}
               className={`${
-                mode === 'auto' ? 'cursor-not-allowed' : 'cursor-pointer'
+                mode === "auto" ? "cursor-not-allowed" : "cursor-pointer"
               } mt-2 px-3 py-0.5 bg-primary-success hover:brightness-95 rounded border-2 border-primary-black`}
             >
               save change
@@ -257,7 +261,7 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
                     queues={queues}
                     setQueues={setQueues}
                   />
-                )
+                );
               })}
             </div>
           </article>
@@ -268,23 +272,23 @@ const Studio: React.FC<Props> = ({ setIsMenuOpen }) => {
             text="Start Livestreaming"
             className="bg-primary-danger"
             onClick={() => {
-              textToSpeech('Halo semuanya apa kabar')
+              textToSpeech("Halo semuanya apa kabar");
             }}
           />
           <ButtonThin text="Pause Livestreaming" className="bg-primary-two" />
           <ButtonThin
             onClick={() => {
-              if (mode === 'auto') setMode('semiauto')
-              if (mode === 'semiauto') setMode('auto')
+              if (mode === "auto") setMode("semiauto");
+              if (mode === "semiauto") setMode("auto");
             }}
-            text={mode === 'auto' ? 'Mode Auto' : 'Mode Semi Auto'}
+            text={mode === "auto" ? "Mode Auto" : "Mode Semi Auto"}
             className="bg-primary-tree"
           />
           <AudioPlayer setQueue={setQueues} queues={queues} />
         </section>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Studio
+export default Studio;
